8de9605a04acbbf90a33fd0f97db76c085061192
AA2908 <abdo200010002@gmail.com>
2020-09-29 15:55:49 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -1,18 +1,16 @@
 ////////////////////////////////////////////////////////////////////     smooze animations    ///////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////        Jquery           ///////////////////////////////////////////////////////////////////
 $('a[href^="#"]').on('click', function(event) {
-
   var target = $( $(this).attr('href') );
-
   if( target.length ) {
       event.preventDefault();
       $('html, body').animate({
           scrollTop: target.offset().top
-      }, 2000);
+      }, 500);
   }
 });
 $("#myBtn").click(function(){ 
-  $("html, body").animate({ scrollTop: 0 }, 2000); 
+  $("html, body").animate({ scrollTop: 0 }, 1000); 
   return false; 
 });
 ////////////////////////////////////////////////////////////////////     play & pause    ////////////////////////////////////////////////////////////////////////
@@ -29,6 +27,22 @@ function myFunction() {
     btn.innerHTML = "Play";
   }
 }
+////////////////////////////////////////////////////////////////////        Fixed Nav    //////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////        Vanila js           ////////////////////////////////////////////////////////////////
+if ($(window).width() > 992) {
+  $(window).scroll(function(){  
+     if ($(this).scrollTop() > 0) {
+        $('#navbar_top').addClass("fixed-top");
+        // add padding top to show content behind navbar
+        $('body').css('padding-top', $('.navbar').outerHeight() + 'px');
+      }else{
+        $('#navbar_top').removeClass("fixed-top");
+         // remove padding top from body
+        $('body').css('padding-top', '0');
+      }   
+  });
+}
+
 ////////////////////////////////////////////////////////////////////        scroll top     //////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////        Vanila js           ////////////////////////////////////////////////////////////////
 var mybutton = document.getElementById("myBtn");
@@ -41,26 +55,136 @@ if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
 }
 ////////////////////////////////////////////////////////////////////        choose schadule     /////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////           Jquery           ////////////////////////////////////////////////////////////////
+// first
 $("#first-butn").click(function(){
-  $("#first-img").animate({
-    width: 'toggle'
-  },1000);
-  });
+  $("#first-butn").addClass("colo");
+  $("#firstDay").addClass("animation-schd");
+  $("#second-butn").removeClass("colo");
+  $("#third-butn").removeClass("colo");
+  $("#forth-butn").removeClass("colo");
+  $("#firstDay").fadeIn("slow");
+  $("#secondDay").hide();
+  $("#forthDay").hide();
+});
+// second and third
+$("#second-butn").click(function(){
+  $("#second-butn").addClass("colo");
+  $("#secondDay").addClass("animation-schd");
+  $("#third-butn").removeClass("colo");
+  $("#first-butn").removeClass("colo");
+  $("#forth-butn").removeClass("colo");
+  $("#secondDay").fadeIn("slow");
+  $("#firstDay").hide();
+  $("#forthDay").hide();
+});
+// second and third
+$("#third-butn").click(function(){
+  $("#third-butn").addClass("colo");
+  $("#secondDay").addClass("animation-schd-left");
+  $("#first-butn").removeClass("colo");
+  $("#second-butn").removeClass("colo");
+  $("#forth-butn").removeClass("colo");
+  $("#secondDay").fadeIn("slow");
+  $("#firstDay").fadeOut("slow");
+  $("#forthDay").fadeOut("slow");
+});
+// forth
+$("#forth-butn").click(function(){
+  $("#forth-butn").addClass("colo");
+  $("#forthDay").addClass("animation-schd");
+  $("#first-butn").removeClass("colo");
+  $("#third-butn").removeClass("colo");
+  $("#second-butn").removeClass("colo");
+  $("#forthDay").fadeIn("slow");
+  $("#firstDay").hide();
+  $("#secondDay").hide();
+});
+////////////////////////////////////////////////////////////////////        ON TYPING TEXT     //////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////        Vanila js           ////////////////////////////////////////////////////////////////
+var words = [
+ "MFN Family is a private owned network for all companies size. We are different from most of the other networks in the market as we offer to our members the revolution of freight forwarders network services. We are definitely a neutral organization, dealing with all members equally and do not provide any forwarding activity at all, but we are providing the administrative management of the network in accordance with the top professional standards."
+];
+var letters = "MFN",
+    speed = 30,
+    steps = 1,
+    loader = document.querySelector('#loader');
 
-  $("#second-butn").click(function(){
-    $("#second-img").animate({
-      width: 'toggle'
-    },1000);
-  });
+function getRandomWord(){
+  var randomWord = words[Math.floor(Math.random() * words.length)];
+  return randomWord;
+}
+function getRandomLetter(){
+  var randomLetter = letters[Math.floor(Math.random() * letters.length)];
+  return randomLetter;
+}
+function randomWordLoop(){
+  var word = getRandomWord();
+  var textLength = word.length;
+  for(var i = 0; i < textLength; i++) {    
+    (function(i,word){
+      letterAppear(i, word);
+    })(i,word)
+  }
   
-  $("#third-butn").click(function(){
-    $("#third-img").animate({
-      width: 'toggle'
-    },1000);
-  });
+  function letterAppear(i, word) {
+    setTimeout(function() {
+      randomLetters(i, word);
+    }, speed*i);  
+  }
 
-  $("#forth-butn").click(function(){
-    $("#forth-img").animate({
-      width: 'toggle'
-    },1000);
-  });
+  function randomLetters(i, word) {
+    for (var j = 0; j <= steps; j++) {
+      charsAnim(i, word, j);
+    }
+  }
+
+  function charsAnim(i, word, j) {
+    setTimeout(function() {
+      var count = j; 
+      if (j < steps) {           
+        randomChar(i, word, count, j);
+      } else {
+        goodChar(i, word, count, j);
+      }
+    }, ((speed/steps)*j) - (speed/steps));
+  }
+
+  function randomChar(i, word, count, j) {
+    var letter = getRandomLetter();    
+    if (j > 0) {
+      var oldText = loader.textContent.slice(0, -1);
+    } else {
+      var oldText = loader.textContent;
+    }
+    loader.textContent = oldText + letter;    
+  }
+  function goodChar(i, word, count, j) {
+    var oldText = loader.textContent.slice(0, -1);  
+    loader.textContent = oldText + word[i];
+    if (i == textLength - 1 ) {
+      removeWord();
+    }
+  }
+  
+  function removeWord() {
+    setTimeout(function() {
+      for (var k = 0; k < textLength; k++) {
+         removeLetters(k);
+      }
+    }, speed*2);
+  }
+  function removeLetters(k) {
+    setTimeout(function() {
+      removeLetter(k);
+    }, 75*k);
+  }
+  function removeLetter(k) {
+    var actualText = loader.textContent.slice(0, -1);
+    loader.textContent = actualText;
+    if (k == textLength - 1) {
+      randomWordLoop();
+    }
+  }
+}
+randomWordLoop();
+
